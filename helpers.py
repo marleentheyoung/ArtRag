5358400db9ef3986e4f39daaaa1f23b0dc211cb1
Marleen de Jonge <marleendejonge@Marleens-MacBook-Pro.local>
2025-07-08 18:24:21 +0100

    data

---
+++ helpers.py
@@ -0,0 +1,19 @@
+def load_data():
+    return [
+        {
+            "file": "starry_night.jpg",
+            "artist": "Vincent van Gogh",
+            "title": "The Starry Night",
+            "year": 1889,
+            "description": "A swirling night sky over a quiet town.",
+            "location": "Museum of Modern Art, New York"
+        },
+        {
+            "file": "mona_lisa.jpg",
+            "artist": "Leonardo da Vinci",
+            "title": "Mona Lisa",
+            "year": 1503,
+            "description": "A portrait of a woman with an enigmatic expression.",
+            "location": "Louvre Museum, Paris"
+        }
+    ]
